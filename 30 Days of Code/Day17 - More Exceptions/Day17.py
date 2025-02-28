c0b0d50e1c9907c4d7357d91cd6c4dc0ad794135
César García <ceegee@outlook.com>
2025-02-27 22:32:27 -0800

    Add solution for HackerRank Day 17: More Exceptions

---
+++ 30 Days of Code/Day17 - More Exceptions/Day17.py
@@ -0,0 +1,23 @@
+# HackerRank: 30 Days of Code: Day 16: More Exceptions
+# Problem: https://www.hackerrank.com/challenges/30-more-exceptions/problem?isFullScreen=true
+
+#Write your code here
+class Calculator():
+    # def __init__(self):
+    def power(self, n, p):
+        if n < 0 or p < 0:
+            raise ValueError("n and p should be non-negative")
+        result = 1
+        for x in range(p):
+            result *= n
+        return result
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)
