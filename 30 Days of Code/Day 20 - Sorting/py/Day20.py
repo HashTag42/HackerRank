0ad4007e8c7ea988229413cf52b1a4fc601e7f3f
César García <ceegee@outlook.com>
2025-03-19 22:04:09 -0700

    Add Day 20 - Sorting projects in Python and Java Updated solution file to include new projects for "Day 20 - Sorting" in both Python and Java. Created Python project file (`py.pyproj`) with bubble sort implementation and added Java file (`Day20Tutorial.java`) for the same functionality. Updated solution configuration to reflect new project settings.

---
+++ 30 Days of Code/Day 20 - Sorting/py/Day20.py
@@ -0,0 +1,36 @@
+# HackerRank: 30 Days of Code: Day 20: Sorting
+# Problem: https://www.hackerrank.com/challenges/30-sorting/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    # Write your code here
+    for i in range(n):
+    # Track number of elements swapped during a single array traversal
+        numberOfSwaps = 0
+
+        for j in range(n-1):
+            # Swap adjacent elements if they are in decreasing order
+            if (a[j] > a[j+1]):
+                temp   = a[j]
+                a[j]   = a[j+1]
+                a[j+1] = temp
+                numberOfSwaps += 1
+
+        if (numberOfSwaps == 0):
+            break
+
+print(f"Array is sorted in {numberOfSwaps} swaps.")
+print(f"First Element: {a[0]}")
+print(f"Last Element: {a[n-1]}")
