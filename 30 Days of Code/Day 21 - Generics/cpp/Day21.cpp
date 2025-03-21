80a0387e2e545054b238b33763bbbc7ad4254212
César García <ceegee@outlook.com>
2025-03-20 19:24:12 -0700

    Add Day 21 - Generics project and implementation
    Updated the solution file to include a new project titled "Day 21 - Generics" with its corresponding C++ project file. Added a new source file `Day21.cpp` that implements a generic function to print array elements.

---
+++ 30 Days of Code/Day 21 - Generics/cpp/Day21.cpp
@@ -0,0 +1,36 @@
+// HackerRank: 30 Days of Code: Day 21: Generics
+// Problem: https://www.hackerrank.com/challenges/30-generics/problem?isFullScreen=true
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+void printArray(const vector<T> v)
+{
+	for(T element: v)
+        cout << element << endl;
+
+	return;
+}
+
+int main()
+{
+	int count;
+
+	cin >> count;
+	vector<int> int_vector(count);
+	for (int i = 0; i < count; i++)
+		cin >> int_vector[i];
+
+	cin >> count;
+	vector<string> string_vector(count);
+	for (int i = 0; i < count; i++)
+		cin >> string_vector[i];
+
+	printArray(int_vector);
+	printArray(string_vector);
+
+	return 0;
+}
