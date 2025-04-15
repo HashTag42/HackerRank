24e9b25f0abc8048cef8730db89d9e936ff2851a
César García <ceegee@outlook.com>
2025-04-15 13:48:31 -0700

    Add "Arrays - DS" challenge solution in  Python

---
+++ Arrays - DS/Arrays-DS.py
@@ -0,0 +1,31 @@
+# HackerRank challenge: https://www.hackerrank.com/challenges/arrays-ds/problem?isFullScreen=true
+
+#!/bin/python3
+
+import os
+
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def reverseArray(a):
+    # Write your code here
+    return list(reversed(a))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+
