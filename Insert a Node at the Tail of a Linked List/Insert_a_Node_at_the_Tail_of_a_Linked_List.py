5695316d2192ed111b42f36733b606fd473511a9
César García <ceegee@outlook.com>
2025-04-15 14:58:31 -0700

    Add "Insert a Node at the Tail of a Linked List" project
    This commit introduces a new project for inserting a node at the tail of a singly linked list, including a Python project file. The solution file (`HackerRank.sln`) is updated to include this project along with its configurations and dependencies. A Python script implementing the required functionality is also added, featuring class definitions and a main execution block for input and output handling.

---
+++ Insert a Node at the Tail of a Linked List/Insert_a_Node_at_the_Tail_of_a_Linked_List.py
@@ -0,0 +1,65 @@
+# HackerRank challenge: https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem?isFullScreen=true
+
+#!/bin/python3
+
+import os
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+ 
+# Complete the insertNodeAtTail function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtTail(head, data):
+    if head is None:
+        head = SinglyLinkedListNode(data)
+        return head
+    else:
+        current = head
+        while current:
+            if current.next is None:
+                node = SinglyLinkedListNode(data)
+                current.next = node
+                return head
+            else:
+                current = current.next
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for i in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtTail(llist.head, llist_item)
+        llist.head = llist_head
+
+    print_singly_linked_list(llist.head, '\n', fptr)
+    fptr.write('\n')
+
+    fptr.close()
+
